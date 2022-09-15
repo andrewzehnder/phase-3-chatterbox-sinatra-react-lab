50823e96da7e38587ecc0cd8cfbdda5417c9a00a
Andrew <azehnder2@gmail.com>
2022-09-14 19:58:21 -0700

    commit tree 08d5fa876b924afeaac33e05eada7e152fe61d16

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
   
 end
